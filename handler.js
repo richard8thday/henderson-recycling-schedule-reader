ccfde829954ae990eb80ed80fb25cbb5cfad2665
Richard Simpson <richard@8thdaysoftware.com>
2017-10-31 13:52:22 -0500

    Added missing inline comments and put some todos in there, too.

--- handler.js
+++ handler.js
@@ -7,10 +7,23 @@ const { extractPickupsFromHTML } = require('./helpers');
 module.exports.getschedule = (event, context, callback) => {
   let pickups;
 
+  // Request html page contents
   request('http://www.hendersontn.org/recycling/recycling.html')
+    // Parse contents and retrieve pickups
     .then(({ data }) => {
       pickups = extractPickupsFromHTML(data);
     })
+    /*
+    // TODO: Read from dynamo, compare, and determine what should happen with new/updated data
+    .then(() => {
+      // Get existing schedules
+
+      // Compare pickup dates
+
+      // TODO: Figure out what to do with mismatched data
+    })
+    */
+    // Write to dynamo
     .then(() => {
       // Save schedule
       return dynamo.put({
@@ -21,6 +34,7 @@ module.exports.getschedule = (event, context, callback) => {
         }
       }).promise();
     })
+    // Write to the console
     .then(() => {
       callback(null, { pickups: pickups });
     })
